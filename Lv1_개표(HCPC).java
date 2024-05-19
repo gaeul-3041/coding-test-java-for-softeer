c1e7ce25f75a8038d8d0882875edff93740325d5
Jin Hyun <pearl5322@gmail.com>
2024-05-19 21:26:52 +0900

    줄맞춤 수정

--- Lv1_개표(HCPC).java
+++ Lv1_개표(HCPC).java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int member = Integer.parseInt(br.readLine());
+	int member = Integer.parseInt(br.readLine());
         
         for(int i = 0; i < member; i++) {
             int num = Integer.parseInt(br.readLine());
